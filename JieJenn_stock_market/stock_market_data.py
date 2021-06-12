a8cf3413d399b71b8f98a7ff310eb1cc30424dc5
Joseph Fischetti <fischettijw@gmail.com>
2021-06-12 09:29:14 -0400

    update prgm_path and file_path vis snippet

--- JieJenn_stock_market/stock_market_data.py
+++ JieJenn_stock_market/stock_market_data.py
@@ -6,25 +6,31 @@ import pandas as pd
 import os
 os.system('cls')
 
-path = f"{os.path.dirname(os.path.realpath(__file__))}\\"
-print(path, '\n')
-# path = f".\JieJenn_stock_market\\"
-ticker = 'IBM'
+ticker = 'GOOGL'
+
 file_name = f'{ticker}.csv'
+path = f"{os.path.dirname(os.path.realpath(__file__))}\\"
+prgm_path = f'{os.path.dirname(os.path.realpath(__file__))}\\'
+print(f'------\n{path}{file_name}\n------')
+
+file_name = f'{ticker}.csv'
+file_path = f'{os.path.dirname(os.path.realpath(__file__))}\\{file_name}'
+
 
 period1 = int(time.mktime(datetime.datetime(2020, 1, 1, 23, 59).timetuple()))
 period2 = int(time.mktime(datetime.datetime(2020, 12, 31, 23, 59).timetuple()))
 interval = '1d'  # 1d, 1wk, 1mo
 
 query_string = f'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={period1}&period2={period2}&interval={interval}&events=history&includeAdjustedClose=true'
-# print(query_string)
 df = pd.read_csv(query_string)
 print(df)
 
-if os.path.exists(f'{path}{file_name}'):
-    os.remove(f'{path}{file_name}')
+
+if os.path.exists(file_path):
+    os.remove(file_path)
     print('remove & create', '\n')
 else:
     print('create', '\n')
 
-df.to_csv(f'{path}{file_name}')  # create
+
+df.to_csv(file_path)
